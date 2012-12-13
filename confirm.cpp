660c2609a198c80e32f2b9f089f1c6c657155f10
L8D <tenorbiel@gmail.com>
2012-12-13 14:34:40 -0600

    Added arguements for custom string

--- confirm.cpp
+++ confirm.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string confirm() {
-  cout << " [Y/n] ";
+string confirm(string confirmText) {
+  cout << confirmText << " [Y/n] ";
   string confirmString;
   cin >> confirmString;
   return confirmString;
   }
-int main () {
-  string confirmString = confirm();
+int main (int argc, char * argv[]) {
+  string confirmString;
+  if (argc > 1) {
+    confirmString = confirm(argv[1]);
+    }
+  else if (argc == 1) {
+    confirmString = confirm("");
+    }
   if (confirmString == "Y" | confirmString == "y") {
     return 0;
   }
